53f6eed0e88c3b4ca3728412ea4d5d08c8f686e3
Ivan Andres Vargas Gutierrez <ivan.vargas@falconfarms.com.co>
2022-04-05 13:20:11 -0500

    prueba dos de process

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-process.env.NODE_TLS_REJECT_UNAUTHORIZED='0'
+//Pruebas process
+process.env.NODE_TLS_REJECT_UNAUTHORIZED='0';
 import app from "./app";
 import './database';
 import { PORT } from "./config";
