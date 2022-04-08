89aab7aff95261bec7cbaff447aae2c410d706b2
Ivan Andres Vargas Gutierrez <ivan.vargas@falconfarms.com.co>
2022-04-08 12:05:47 -0500

    regresando puerto 3000

--- src/config.js
+++ src/config.js
@@ -3,4 +3,4 @@ import { config } from "dotenv";
 config();
 export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/test";
 
-export const PORT = process.env.PORT || 4000;
+export const PORT = process.env.PORT || 3000;
