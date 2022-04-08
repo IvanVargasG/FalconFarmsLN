c17255a2f7f85743867c9b4eb835c1bf6069c06e
Ivan Andres Vargas Gutierrez <ivan.vargas@falconfarms.com.co>
2022-04-08 09:55:35 -0500

    agregando nuevo process

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import app from "./app";
 import './database';
 import { PORT } from "./config";
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 app.listen(PORT);
 
 console.log("Server on port", PORT);
